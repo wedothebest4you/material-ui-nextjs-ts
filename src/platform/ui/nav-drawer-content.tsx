e686705c382e2ae2e993b95d1ce4715531eae2b1
wedothebest4you <wedothebest4you@gmail.com>
2026-09-14 13:03:19 +0530

    Develop navigation drawer

--- src/platform/ui/nav-drawer-content.tsx
+++ src/platform/ui/nav-drawer-content.tsx
@@ -6,36 +6,72 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListSubheader from '@mui/material/ListSubheader';
 import ListItemButton from '@mui/material/ListItemButton';
 import Collapse from '@mui/material/Collapse';
-import { ReactElement } from 'react';
+import { useState } from 'react';
 
 // find out the first level parent nodes
 
 // print each parent node
 export default function NavDrawerContent() {
-  let listContent: Array<ReactElement> = [];
-  const erpModules = RoutesRegistry['erp'].toplevelnav;
-  erpModules.children.forEach((moduleId) => {
-    // console.log(erpModules.description);
-    const moduleTopLvlRoutes = RoutesRegistry[moduleId].toplevelnav;
-    // modules are always expanded, so no collapse composer here
-    listContent.push(
-      <ListItemButton>
-        <ListItemText>{moduleTopLvlRoutes.description}</ListItemText>,
-      </ListItemButton>,
-    );
+  const [contentOpenStates, setContentOpenStates] = useState<
+    Record<string, boolean>
+  >({});
+  // Which shape data would be in the variable erpModules ?
+  // the variable erpModules will be a single object as shown in the below
+  // sample. furthermore, it is the root object of route registry.
+  // it stores the list of all modules in the erp system.
+  // Sample :
+  // {
+  //   description: 'erp all',
+  //   children: ['finance', 'platformadm'],
+  // },
+  // for more, refer to routes-registry.ts:
 
-    moduleTopLvlRoutes.children.forEach((routeId) => {
-      listContent.push(
+  const erpModules = RoutesRegistry['erp'].toplevelnav;
+
+  // Which shape of data will be in the variable moduleTopLvlRoutes ?
+  // the variable moduleTopLvlRoutes will be an array of
+  // objects of top level navigators in each module.
+  // Furthermore, each module will have its own top level navigators.
+  // Below is an instance of moduleTopLvlRoutes,
+  // it shows the two top level navigators for finance and platfrom admm modules.
+  // Sample :
+  // [
+  //   {
+  //     moduleId : 'finance',
+  //     description: 'Finance',
+  //     children: ['dashboards', 'masters'],
+  //   },
+  //   {
+  //     moduleId : 'platformadmn',
+  //     description: 'Platform Admin',
+  //     children: ['dashboards', 'masters'],
+  //    },
+  // ]
+  // for more, refer to financeMdule.ts and module.ts
+
+  const moduleTopLvlRoutes = erpModules.children.map((moduleId) => ({
+    moduleId,
+    ...RoutesRegistry[moduleId].toplevelnav,
+  }));
+
+  const listContent = moduleTopLvlRoutes.map(
+    ({ moduleId, description, children }) => (
+      <>
+        <ListItemButton>
+          <ListItemText>{description}</ListItemText>
+        </ListItemButton>
         <Collapse in={true}>
-          <RouteAndSubroute
-            key={moduleId + routeId}
-            moduleId={moduleId}
-            routeId={routeId}
-          />
-        </Collapse>,
-      );
-    });
-  });
+          {children.map((routeId) => (
+            <RouteAndSubroutes
+              key={moduleId + routeId}
+              moduleId={moduleId}
+              routeId={routeId}
+            />
+          ))}
+        </Collapse>
+      </>
+    ),
+  );
   return (
     <List subheader={erpModules.description} dense>
       {listContent}
@@ -43,7 +79,7 @@ export default function NavDrawerContent() {
   );
 }
 
-function RouteAndSubroute({
+function RouteAndSubroutes({
   moduleId,
   routeId,
 }: {
@@ -53,39 +89,162 @@ function RouteAndSubroute({
   // Logic : Print the given node with its childen.
   // Note : if any of the child has own children, then call this
   // code recursively treating that child as parent
-  let listContent: Array<ReactElement> = [];
+
+  // let us say we have the following test data:
+  // {
+  //   finance: {
+  //     toplevelnav: {
+  //       description: 'Finance',
+  //       children: ['dashboards', 'masters'],
+  //     },
+  //     dashboards: {
+  //       description: 'Dashboards',
+  //       children: ['4'],
+  //     },
+  //     masters: {
+  //       description: 'Masters',
+  //       children: ['acctemplate', '5'],
+  //     },
+  //     acctemplate: {
+  //       description: 'Account Template',
+  //       children: ['1', '2', '3'],
+  //     },
+  //     4: {
+  //       description: 'Overview - dashboard',
+  //       fullPath: '/finance',
+  //       component: () => import('../ui/dashboard'),
+  //     },
+  //     1: {
+  //       description: 'Account Template - Create',
+  //       fullPath: '/finance/coa',
+  //       component: () =>
+  //         import('../chart-of-accounts/account-template/ui/account-template-grid'),
+  //     },
+  //     2: {
+  //       description: 'Account Template - Edit',
+  //       fullPath: '/finance/coa',
+  //       component: () =>
+  //         import('../chart-of-accounts/account-template/ui/account-template-grid'),
+  //     },
+  //     3: {
+  //       description: 'Account Template - Delete',
+  //       fullPath: '/finance/coa',
+  //       component: () =>
+  //         import('../chart-of-accounts/account-template/ui/account-template-grid'),
+  //     },
+  //     5: {
+  //       description: 'Create Ledger',
+  //       fullPath: '/finance/ledger/new',
+  //       component: () => import('../ui/LedgerCreateForm'),
+  //     },
+  //   },
+  // };
+  //
+  // eg. simple case
+  // This function has been invoked with the args - module : finance, route: dashboards.
+  // The dashboards route has children or sub routes. Therefore, the map method will
+  // invoke. No child of dashboards has no more children.
+  // Therefore the nested declaration will be SKIPPED, and the route
+  // Overview - dashboard will be found as its only one list content.
+  // This will result the following JSX when this function will return.
+  // <Collapse>
+  //   <List>
+  //     <ListItemButton>
+  //       <ListItemText>Dashboards</ListItemText>
+  //     </ListItemButton>
+  //     <Collapse>
+  //       <List>
+  //         <ListItemButton>
+  //           <ListItemText>Overview - dashboard</ListItemText>
+  //         </ListItemButton>
+  //       </List>
+  //     </Collapse>
+  //   </List>
+  // </Collapse>;
+  //
+  // eg. recursive case
+  // module : finance, route : masters.
+  // The first call to this function, will see masters route
+  // has children or sub routes as 'acctemplate' and '5', therefore, the map method will
+  // invoke. The child 'acctemplate' has FUTHER CHILDREN,
+  // therefore the nested declaration will be invoked with 'acctemplate' as the route.
+  // Please note, in this nested call, the route is 'acctemplate'. And it has children '1','2' and '3'.
+  // However, non of these child has FURTHER CHILDREN. Therefore, there is no more nested calls.
+  // instead each child will be mapped to the respective JSX.
+  // This will result an array of the following JSX.
+  // [
+  //  <ListItemButton><LiteItemText>AccountTemplate - Create</LiteItemText></ListItemButton>,
+  //  <ListItemButton><LiteItemText>AccountTemplate - Edit</LiteItemText></ListItemButton>,
+  //  <ListItemButton><LiteItemText>AccountTemplate - Delete</LiteItemText></ListItemButton>,
+  // ]
+  // when the call will return, the final JSX will be the following.
+  // <Collapse>
+  //   <List>
+  //     <ListItemButton>
+  //       <ListItemText>Account Template</ListItemText>
+  //     </ListItemButton>
+  //     <Collapse>
+  //       <List>
+  //         <ListItemButton>
+  //           <ListItemButton>
+  //             <ListItemText>AccountTemplate - Create</ListeItemText>
+  //           </ListItemButton>
+  //           ,
+  //           <ListItemButton>
+  //             <ListItemText>AccountTemplate - Edit</ListeItemText>
+  //           </ListItemButton>
+  //           ,
+  //           <ListItemButton>
+  //             <ListItemText>AccountTemplate - Delete</ListeItemText>
+  //           </ListItemButton>
+  //           ,
+  //         </ListItemButton>
+  //       </List>
+  //     </Collapse>
+  //   </List>
+  // </Collapse>;
+  //
+  // BUT the story of masters is not over, it has one more child as '5'.
+  // However this has no futher children, therefore no more nested declaration.
+  // So the JSX for this child alone will be the following.
+  //  <ListItemButton>
+  //    <ListItemText>Create Ledger</ListItemText>
+  //  </ListItemButton>
+  // Now the whole JSX for the main route 'masters' will include the jsx of the two child routes.
+  // Therefore, the final JSX for the route masters will be the following.
+  // <Collapse>
+  //   <List>
+  //     <ListItemButton>
+  //       <ListItemText>Masters</ListItemText>
+  //     </ListItemButton>
+  //     <Collapse>
+  //       <List>
+  //         // the merged JSX of 'acctemplate' and '5' will come here
+  //         // for brevity, it has been not reproduced here.
+  //       </List>
+  //     </Collapse>
+  //   </List>
+  // </Collapse>;
+
   const value = RoutesRegistry[moduleId][routeId];
   // console.log(RouteReg.getModuleRegistry()[key]);
   // console.log(value);
 
-  if (Array.isArray(value.children)) {
-    value.children.map((child) => {
-      if (Array.isArray(RoutesRegistry[moduleId][child].children)) {
-        listContent.push(
-          <RouteAndSubroute
-            key={moduleId + child}
-            moduleId={moduleId}
-            routeId={child}
-          />,
-        );
-      } else {
-        // console.log(
-        //   moduleId,
-        //   ':',
-        //   value.description,
-        //   ':',
-        //   RoutesRegistry[moduleId][child].description,
-        //);́
-        listContent.push(
-          <ListItemButton>
-            <ListItemText>
-              {RoutesRegistry[moduleId][child].description}
-            </ListItemText>
-          </ListItemButton>,
-        );
-      }
-    });
-  }
+  const listContent = value.children?.map((child) =>
+    Array.isArray(RoutesRegistry[moduleId][child].children) ? (
+      <RouteAndSubroutes
+        key={moduleId + child}
+        moduleId={moduleId}
+        routeId={child}
+      />
+    ) : (
+      <ListItemButton>
+        <ListItemText>
+          {RoutesRegistry[moduleId][child].description}
+        </ListItemText>
+      </ListItemButton>
+    ),
+  );
   /*
  each node expanded here are already under the respective modules
  therefore there is an outer collapse. And each node resulted by expansion
